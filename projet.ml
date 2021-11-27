67ccccb4ec920f9f7fff6a9fe82f98d5ffc88870
Eve Poitevin <poitevineve@gmail.com>
2021-11-27 20:38:16 +0100

    1.1.1, 1.1.2, 1.1.3 réalisés, inspirés de la grammaire donnée dans le cours

--- projet.ml
+++ projet.ml
@@ -4,11 +4,96 @@
 
 (* 1.1.1 *)
 
-type variable = A | B | C | D;;
+(* Grammaire
 
-type booleen = 0 | 1;;
+   Variables :
+   V ::= a | b | c | d (variable)
+   B ::= 0 | 1 (booléen)
+   E ::= N | B (expression)        
 
-type 
+   Instructions :
+   I ::=
+     | I;I
+     | V:=E
+     | w(E){I}
+     | i(E){I}{I}
+     | epsilon
+     
+ *)
 
+type v =
+  | A
+  | B
+  | C
+  | D;;
 
+type b =
+  | Zero
+  | Un;;
 
+type e =
+  | Variable of v
+  | Booleen of b;;
+
+type i =
+  | Sequence of i * i
+  | Affectation of v * e
+  | While of e * i
+  | If of e * i * i
+  | EpsilonI;;
+
+(* 1.1.2 *)
+
+(* Grammaire
+
+   Variables :
+   V ::= a | b | c | d (variable)
+   B ::= 0 | 1 (booléen)
+   E ::= N | B (expression)        
+
+   Instructions :
+   I ::=
+     | I;I
+     | V:=E
+     | w(E){I}
+     | i(E){I}{I}
+     | epsilon
+     
+ *)
+
+(* 1.1.3 *)
+
+(* Nouvelle grammaire
+
+   Variables :
+   V ::= a | b | c | d (variable)
+   B ::= 0 | 1 (booléen)
+   E ::= N | B (expression)        
+
+   Instructions :
+
+   S ::= IL | epsilon (axiome)
+   L ::= ;S | epsilon (liste d'instructions)
+   I ::= 
+   | V:=E 
+   | i(E){S}{S} 
+   | w(E){S}    
+   | epsilon
+   
+ *)
+
+(* Nouveau type i *)
+
+type s =
+  | Axiome of i * l
+  | EpsilonS
+and
+  l =
+  | ListeInstructions of s
+  | EpsilonL
+and
+  i =
+  | Affectation of v * e
+  | While of e * s
+  | If of e * s * s
+  | EpsilonI;;
